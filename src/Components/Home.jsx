c3e152ca199f888033aa3daf3606854bd03950b5
Allan Odhiambo <odhiamboallan002@gmail.com>
2025-04-19 12:15:07 +0300

    add

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
           backgroundImage: `url(./Image/nairobi-expressway.jpg)`,
         }}
       >
-        <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center">
+        <div className="absolute inset-0 bg-opacity-60 flex items-center justify-center">
           <div className="text-center text-white px-6">
             <h1 className="text-5xl md:text-6xl font-bold mb-6">
               Find Your Dream Home in Nairobi & Kiambu
